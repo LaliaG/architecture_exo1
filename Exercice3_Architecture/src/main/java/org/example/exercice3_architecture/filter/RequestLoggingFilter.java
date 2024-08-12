d3b60cdc4d9d191c95f6fcc6a1b5c46d5822870d
Lalia SY <laliasy@yahoo.fr>
2024-08-12 17:00:37 +0200

    Exo3

--- Exercice3_Architecture/src/main/java/org/example/exercice3_architecture/filter/RequestLoggingFilter.java
+++ Exercice3_Architecture/src/main/java/org/example/exercice3_architecture/filter/RequestLoggingFilter.java
@@ -9,19 +9,38 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Component
 public class RequestLoggingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String uri = request.getRequestURI();
+
+        String requestURI = request.getRequestURI();
+        String httpMethod = request.getMethod();
+        String clientIP = request.getRemoteAddr();
+        LocalDateTime requestTime = LocalDateTime.now();
+
+
+        System.out.println("Requête reçue :");
+        System.out.println("URI : " + requestURI);
+        System.out.println("Méthode HTTP : " + httpMethod);
+        System.out.println("Timestamp : " + requestTime);
+        System.out.println("Adresse IP du client : " + clientIP);
+       /* String uri = request.getRequestURI();
         String method = request.getMethod();
         String clientIp = request.getRemoteAddr();
         Instant timestamp = Instant.now();
 
-        System.out.println("Requête reçue - URI: " + uri + ", Méthode: " + method + ", IP du client: " + clientIp + ", Timestamp: " + timestamp);
+        System.out.println("Requête reçue :");
+        System.out.println("URI : " + requestURI);
+        System.out.println("Méthode HTTP : " + httpMethod);
+        System.out.println("Timestamp : " + requestTime);
+        System.out.println("Adresse IP du client : " + clientIP);
 
+       // System.out.println("Requête reçue - URI: " + uri + ", Méthode: " + method + ", IP du client: " + clientIp + ", Timestamp: " + timestamp);
+*/
         filterChain.doFilter(request, response);
     }
 }
